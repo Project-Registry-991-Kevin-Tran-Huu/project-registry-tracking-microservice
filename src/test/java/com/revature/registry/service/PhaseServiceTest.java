e580f33b8e41788b10067ecd81a8e9885f77e897
devaraj1234 <dev.acharya286@gmail.com>
2021-06-14 14:05:07 -0500

    created the classes for the phase service test and phase controller test

--- src/test/java/com/revature/registry/service/PhaseServiceTest.java
+++ src/test/java/com/revature/registry/service/PhaseServiceTest.java
@@ -1,5 +1,28 @@
 package com.revature.registry.service;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.revature.registry.ProjectRegistryTrackingApplication;
+import com.revature.registry.repository.PhaseRepository;
+
+@SpringBootTest(classes = ProjectRegistryTrackingApplication.class)
+@ExtendWith(SpringExtension.class)
 public class PhaseServiceTest {
+	
+	@Autowired
+    private PhaseService phaseService;
+	
+	@Mock
+	private PhaseRepository phaseRepo;
+	
+	@BeforeEach
+	public static void setup() {
+		
+	}
 
 }
