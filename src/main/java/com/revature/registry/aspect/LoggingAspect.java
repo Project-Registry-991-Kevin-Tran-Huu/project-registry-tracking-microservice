9d66e57d0e2d5b78e02913d3164f8854ccdb7328
devaraj1234 <dev.acharya286@gmail.com>
2021-06-17 15:02:54 -0500

    testing for web hook

--- src/main/java/com/revature/registry/aspect/LoggingAspect.java
+++ src/main/java/com/revature/registry/aspect/LoggingAspect.java
@@ -9,11 +9,11 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class LoggingAspect {
-	
+	/*
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 	
 	@Pointcut
 	public void springBeanPointcut() {
     }
-
+*/
 }
